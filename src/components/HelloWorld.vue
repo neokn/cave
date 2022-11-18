5d40403834b67c46de0a0ecc5ac478455383cb77
Neo Kusanagi <jr@neo.kn>
2022-11-18 10:48:31 +0800

    fix style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,4 +23,24 @@ const echo = () => {
 .read-the-docs {
   color: #888;
 }
+input {
+    padding: 5px 12px;
+    font-size: 14px;
+    line-height: 20px;
+    color: #24292e;
+    vertical-align: middle;
+    background-color: #ffffff;
+    background-repeat: no-repeat;
+    background-position: right 8px center;
+    border: 1px solid #e1e4e8;
+    border-radius: 6px;
+    outline: none;
+    box-shadow: rgba(225, 228, 232, 0.2) 0px 1px 0px 0px inset;
+
+}
+input:focus{
+    border-color: #0366d6;
+    outline: none;
+    box-shadow: rgba(3, 102, 214, 0.3) 0px 0px 0px 3px;
+}
 </style>
