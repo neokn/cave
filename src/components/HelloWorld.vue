ac966db417c8dcdc8742127a9f80918b953cb8e6
Neo Kusanagi <jr@neo.kn>
2022-11-18 10:43:48 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const ping = ref("")
+
+const echo = () => {
+    alert(ping.value.toUpperCase())
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <input type="text" v-model="ping">
+    <button type="button" @click="echo()">shout</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
